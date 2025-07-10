c8a9f7d1e87b139f51be37e1bdb1b12943c814e7
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:02:05 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "IRIS_1.0" `
+  -ModName "IRIS 1.0" `
+  -ModFolder "iris" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/iris/Iris-1.0-Original.zip" `
+  -ModBaseFilesUrlHash "515d5e2c5fe5e4ab1233b8925ecdbe92387941d9805184208e9dd501e9278272" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
